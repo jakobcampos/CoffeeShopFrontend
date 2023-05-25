cc64b0c4816476048659467e04542b9664126273
Jakob Campos <104319479+jakobcampos@users.noreply.github.com>
2023-05-25 12:36:20 -0700

    Added searchbar and cart buttons

--- coffee_app/lib/screens/home/home.dart
+++ coffee_app/lib/screens/home/home.dart
@@ -1,1 +1,31 @@
+import 'package:coffee_app/constants/colors.dart';
+import 'package:flutter/material.dart';
 
+import '../../widgets/custom_app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kBackground,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar(
+            Icons.shopping_bag_outlined,
+            Icons.search_outlined,
+          )
+        ],
+      ),
+    );
+  }
+}
