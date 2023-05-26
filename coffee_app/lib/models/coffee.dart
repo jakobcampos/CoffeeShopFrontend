efd268ea37eeacda87980df95aac5f19d2d6ec87
Jakob Campos <104319479+jakobcampos@users.noreply.github.com>
2023-05-26 01:45:08 -0700

    Added More Coffees To Recommended List

--- coffee_app/lib/models/coffee.dart
+++ coffee_app/lib/models/coffee.dart
@@ -23,6 +23,34 @@ class Coffee {
             {'Beans': 'assets/images/coffee-beans.png'},
           ],
           hightLight: true),
+      Coffee('assets/images/coffee-cup.png', 'Cappuccino', '150 kcal', 4.5, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
+      Coffee(
+          'assets/images/coffee-cup.png', 'Mocha Latte', '280 kcal', 5.0, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Chocolate': 'assets/images/chocolate.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
+      Coffee('assets/images/coffee-cup.png', 'Caramel Frappacino', '280 kcal',
+          8.0, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Chocolate': 'assets/images/chocolate.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
+      Coffee('assets/images/coffee-cup.png', 'Iced White Mocha', '280 kcal',
+          6.35, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Chocolate': 'assets/images/chocolate.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
+      Coffee(
+          'assets/images/coffee-cup.png', 'Matcha Latte', '280 kcal', 4.5, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Chocolate': 'assets/images/chocolate.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
     ];
   }
 
@@ -38,6 +66,13 @@ class Coffee {
             {'Beans': 'assets/images/coffee-beans.png'},
           ],
           hightLight: true),
+      Coffee('assets/images/coffee-cup.png', 'Espresso', '75 kcal', 3.0, 1, [
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
+      Coffee('assets/images/coffee-cup.png', 'Macchiato', '100 kcal', 4.0, 1, [
+        {'Milk': 'assets/images/milk.png'},
+        {'Beans': 'assets/images/coffee-beans.png'},
+      ]),
     ];
   }
 }
